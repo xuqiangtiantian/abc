809268420e6f91b441adf7c697ed2b37c0590b12
xuqiangtiantian <261186306@qq.com>
2019-01-15 23:24:07 +0800

    aaaa

--- ideawork/testgitlab/src/main/java/cn/xuqiang/gitlab/TestMain.java
+++ ideawork/testgitlab/src/main/java/cn/xuqiang/gitlab/TestMain.java
@@ -5,5 +5,6 @@ public class TestMain {
     public static void main(String[] args) {
         System.out.println("dfdf");
         System.out.println("dfdf");
+        System.out.println("aa");
     }
 }
